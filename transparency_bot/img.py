727f467e631df3226f8b32e0bf780f59efd7f1fe
idmyn <hello@davidmyno.rs>
2020-03-01 17:38:20 +0000

    make black and white pixels transparent

--- transparency_bot/img.py
+++ transparency_bot/img.py
@@ -10,7 +10,8 @@ def white_to_transparent(raw_image):
     datas = resized.getdata()
     newData = []
     for item in datas:
-        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+        if ((item[0] == 255 and item[1] == 255 and item[2] == 255) or
+            (item[0] == 0 and item[1] == 0 and item[2] == 0)):
             newData.append((255, 255, 255, 0))
         else:
             newData.append(item)
